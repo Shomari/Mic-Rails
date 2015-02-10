7ac46bac985829baa7401e5845ae177ae3d6df03
Shomari <Shomari.Ewing@gmail.com>
2015-02-09 23:34:43 -0600

    fixed up create players console page

---
+++ app/assets/javascripts/new.js
@@ -0,0 +1,27 @@
+$( document ).ready(function(){
+	$("#XBL").hide();
+	$("#PSN").hide();
+	$("#Xbox_One").on("click", function(){
+		$("#XBL").toggle();
+	});
+	$("#Playstation_4").on("click", function(){
+		$("#PSN").toggle();
+	});
+	$("form").submit(function(event){
+		if(!atLeastOneChecked()){
+			event.preventDefault();
+			$("p").hide();
+			$("#create_form").before("<p style='color:red'>At least one tag must be supplied</p>");
+		};
+	});
+});
+
+function atLeastOneChecked(){
+	if($("#XBL").val().length > 0){
+		return true;
+	} else if($("#PSN").val().length > 0) {	
+		return true;
+	} else {
+		return false;
+	}
+};
