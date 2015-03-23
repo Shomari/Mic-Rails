0ad923f408efa202e1b62eaa5ec05f0eb77c40af
Shomari <Shomari.Ewing@gmail.com>
2015-03-23 00:48:33 -0500

    more spec

--- spec/models/answer_book_spec.rb
+++ spec/models/answer_book_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
+require 'shoulda/matchers'
 
 RSpec.describe AnswerBook, :type => :model do
+
+	it {should belong_to(:player)}
+	it {should belong_to(:question)}
+	it {should belong_to(:answer)}
+
 end
